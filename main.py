b4d409be5f7eaad8c2181baf6b410a1218491379
Artjoms Fedins <tjomaf2018@gmail.com>
2025-07-19 16:54:52 +0300

    Add template index.html

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template
 
+import sqlite3
+
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-    return render_template("index.html")
+    connection = sqlite3.connect("data.db")
+    cursor = connection.cursor()
+
+    data = cursor.execute("SELECT * FROM portfolio").fetchall()
+    print(data)
+    connection.commit()
+    connection.close()
+    return render_template("index.html", data=data)
 
 
 if __name__ == "__main__":
