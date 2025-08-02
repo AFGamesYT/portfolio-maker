ce9bfd18b28248e1e6edeb7a3863f8a46de1645e
Artjoms <a.f.gamesyt123@gmail.com>
2025-08-02 11:41:26 +0300

    added personal porfolio links
    and more

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request, redirect, url_for
+
+import uuid
+from werkzeug.utils import secure_filename
 
 import sqlite3
 
@@ -21,11 +25,133 @@ def homepage():
     cursor = connection.cursor()
 
     data = cursor.execute("SELECT * FROM portfolio").fetchall()
-    print(data)
+
     connection.commit()
     connection.close()
     return render_template("index.html", data=data)
 
+@app.route("/create/", methods=["GET", "POST"])
+def create_portfolio():
+    if request.method == "GET":
+        return render_template("form.html")
+
+    if request.method == "POST":
+        error = False
+        errors = []
+
+        try:
+            connection = sqlite3.connect("data.db")
+            cursor = connection.cursor()
+
+
+            existing_portfolios = cursor.execute("SELECT * FROM portfolio").fetchall()
+            existing_names = []
+            existing_uuids = []
+
+            for existing_portfolio in existing_portfolios:
+                existing_names.append(existing_portfolio[2])
+                existing_uuids.append(existing_portfolio[1])
+
+            print(f"Existing Names: {existing_names}, Existing Uuids: {existing_uuids}")
+
+            data = request.form
+
+            username = data['username']
+            if username in existing_names:
+                error = True
+                errors.append("This username is taken.")
+
+
+            bio = data['bio']
+            skills = data['skills']
+            github_username = data['github_username'].strip().replace("https://github.com/", '').replace('/', '')
+
+            check_req = requests.get(f"https://api.github.com/users/{github_username}/repos")
+            if check_req.status_code == 404:
+                error = True
+                errors.append("This GitHub username does not exist. Please write the link to it, or the GitHub username directly.")
+
+            telegram = data['telegram']
+
+
+            uid = str(uuid.uuid4())
+            if uid in existing_uuids:
+                uid = str(uuid.uuid4())
+
+
+            avatar = request.files.get('avatar')
+            if not error and avatar and avatar.filename:
+                filename = secure_filename(f"{uid}_{avatar.filename}")
+                while "/" in filename:
+                    filename.replace("/", "")
+
+                avatar_path = f"static/uploads/{filename}"
+                avatar.save(avatar_path)
+
+                avatar_filename = avatar_path.replace("static/", "")
+            else:
+                avatar_filename = 'placeholder.png'
+
+            if not error:
+                cursor.execute("""
+                INSERT INTO portfolio (uuid, name, bio, github, telegram, avatar, skills) 
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+                """, (uid, username, bio, github_username, telegram, avatar_filename, skills))
+            else:
+                print(errors)
+
+            connection.commit()
+            connection.close()
+
+            return redirect(url_for("homepage"))
+
+
+        except Exception:
+            error = True
+            errors.append("Something went wrong. Please try again later.")
+            print(errors)
+
+
+@app.route('/portfolio/<user_uuid>')
+def view_portfolio(user_uuid):
+    connection = sqlite3.connect("data.db")
+    cursor = connection.cursor()
+
+    data = cursor.execute("SELECT * FROM portfolio WHERE uuid = ?", (user_uuid,)).fetchone()
+    if not data:
+        return "404: This portfolio doesn't exist.", 404
+
+    skills = str(data[7]).split(", ")
+
+    gh_repos = []
+
+    gh_username = data[4]
+    if gh_username:
+        gh_data = requests.get(f"https://api.github.com/users/{gh_username}/repos")
+        if not gh_data.ok:
+            return "Something went wrong. Please check your connection and try again."
+
+
+        for repo in gh_data.json():
+            if repo["id"] and repo["name"]:
+                if not repo["private"]:
+                    gh_repos.append({"name": repo["name"], "description": repo["description"], "link": repo["html_url"]})
+
+    connection.close()
+
+    data_dict = {
+        "id": data[0],
+        "uuid": data[1],
+        "name": data[2],
+        "bio": data[3],
+        "github": data[4],
+        "telegram": data[5],
+        "avatar": data[6],
+        "skills": skills,
+        "gh_repos": gh_repos
+    }
+
+    return render_template("portfolio_template.html", **data_dict)
 
 if __name__ == "__main__":
     app.run()
